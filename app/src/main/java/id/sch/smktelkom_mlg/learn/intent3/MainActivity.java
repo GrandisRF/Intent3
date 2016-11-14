a90cef95f57080808af14714a31a0b677896981a
Grandis Rufaidah <grandisrufaidah99@gmail.com>
2016-11-14 04:13:07 -0800

    Intent Camera (Photo)

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -42,8 +42,33 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
+        findViewById(R.id.imageViewCamera)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        capturePhoto();
+                    }
+                });
+
 
     }
+
+    private void capturePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bitmap bitmap = data.getParcelableExtra("data");
+            ImageView iv = (ImageView) findViewById(R.id.imageViewCamera);
+            iv.setImageBitmap(bitmap);
+        }
+    }
+
 
     private void openWebPage(String url) {
         Uri webpage = Uri.parse(url);
